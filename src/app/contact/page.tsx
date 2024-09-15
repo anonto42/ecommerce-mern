64264e7a744b3d4a649eea78f902205f9e8d8cc7
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-15 16:16:32 +0600

    content updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,18 @@
+"use client"
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full min-h-screen'>page</div>
+    <div style={{backgroundImage:`url("/Image/bg-se.jpg")`}} className='w-full min-h-screen bg-cover'>
+      <div className='max-w-[1400px] h-full mx-auto pt-[20vh]'>
+        <div className=''>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorum aspernatur cupiditate praesentium quos odio quisquam libero totam. Neque, alias!
+        </div>
+        <div className='w-[full] h-[400px] border-2 mt-[20vh] border-white'>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
