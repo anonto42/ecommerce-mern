5cd34c66caa0c4a9640d32fd8002755bf516c086
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-15 15:38:15 +0600

    component

---
+++ src/app/(Home)/Lib/Cont.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Cont = () => {
+  return (
+    <div className="w-full h-screen bg-slate-600">
+        <div className='max-w-[1400px] mx-auto'></div>
+    </div>
+  )
+}
+
+export default Cont
