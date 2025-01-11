ce6aff608a5ccaa954e060f46d7790fe7662ba87
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-11 15:58:39 +0600

    redux configed

--- src/Components/Naveber/Naveber.tsx
+++ src/Components/Naveber/Naveber.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setLocation } from '@/Redux/counters/LocationSlice';
+import { usePathname } from 'next/navigation';
 
 const Naveber = () => {
+  const dispach = useDispatch()
+  const partname = usePathname();
+
+  useEffect(()=>{
+    dispach(setLocation(partname))
+  },[])
+
   return (
-    <div>Naveber</div>
+    <div>
+    </div>
   )
 }
 
