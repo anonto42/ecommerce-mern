a9a2d7396bddccb00dbe51d80dc6fef046aded03
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-11 14:52:04 +0600

    home page

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { usePathname } from "next/navigation";
 
 const page = () => {
+  
+  const partname = usePathname()
+  console.log(partname)
+
   return (
     <div>page</div>
   )
