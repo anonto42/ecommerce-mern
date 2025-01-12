f6162fedcb7acc25a0720855e5a04d4b4d3b3760
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-12 21:45:46 +0600

    color file

---
+++ src/utils/colors.ts
@@ -0,0 +1,8 @@
+
+
+
+export const IconColor:string = "#2db12d"
+export const Icon2ndColor:string ="#c9c9c9"
+export const TopTextOrTheFontColor:string = "#ffffff"
+
+export const TopBarBgColor:string ="#202124"
