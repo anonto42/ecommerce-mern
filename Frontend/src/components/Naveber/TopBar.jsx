f01ec57bdcbd2fc744875e6212ae36f428826c1e
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-13 19:07:11 +0600

    style updated

--- Frontend/src/components/Naveber/TopBar.jsx
+++ Frontend/src/components/Naveber/TopBar.jsx
@@ -5,60 +5,59 @@ import { FaShoppingCart } from "react-icons/fa";
 import { MdOutlineFavorite } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import { mainIconColor, sndIconColor, topBarBgColor, topBarTextColor } from '../../utils/colors';
 
 const TopBar = () => {
   return (
-    <div className={`w-full bg-[${topBarBgColor}]`}>
+    <div className={`w-full bg-mainBg`}>
         <div className='flex mx-auto h-[60px] max-w-[1280px] justify-between items-center px-6 sm:px-8 md:px-10  xl:px-0 duration-150 ease-in-out'>
             <div className='flex'>
                 <Link to={"/"} className='flex text-center xl:mr-3'>
                     <div 
-                        className={`border-2 border-[${sndIconColor}] hover:border-none hover:bg-[${mainIconColor}] w-[35px] h-[35px] rounded-full flex justify-center items-center text-[${mainIconColor}] hover:text-[${sndIconColor}] ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0`}
+                        className={`border-2 border-sndIconColor hover:border-none hover:bg-mainIconColor w-[35px] h-[35px] rounded-full flex justify-center items-center text-mainIconColor hover:text-sndIconColor ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0`}
                         title='Contact number'
                         >
                         <IoIosCall size={24}/>
                     </div>
-                    <h2 className={`hidden xl:block text-[${topBarTextColor}] font-semibold mt-1`}>+88-01864461331</h2>
+                    <h2 className={`hidden xl:block text-topBarTextColor font-semibold mt-1`}>+88-01864461331</h2>
                 </Link>
                 <Link to={""} className='flex text-center xl:mr-3'>
                     <div 
-                        className={`border-2 border-[${sndIconColor}] hover:border-none hover:bg-[${mainIconColor}] w-[35px] h-[35px] rounded-full flex justify-center items-center text-[${mainIconColor}] hover:text-[${sndIconColor}] ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0`}
+                        className={`border-2 border-sndIconColor hover:border-none hover:bg-mainIconColor w-[35px] h-[35px] rounded-full flex justify-center items-center text-mainIconColor hover:text-sndIconColor ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0`}
                         title='E-Mail'
                     >
                         <IoMail size={23} />
                     </div>
-                    <h2 className={`hidden xl:block text-[${topBarTextColor}] font-semibold mt-1`}>anontom90@gmail.com</h2>
+                    <h2 className={`hidden xl:block text-topBarTextColor font-semibold mt-1`}>anontom90@gmail.com</h2>
                 </Link>
                 <Link to="" className='xl:hidden'>
                     <div 
-                        className={`border-2 border-[${sndIconColor}] hover:border-none hover:bg-[${mainIconColor}] w-[35px] h-[35px] rounded-full flex justify-center items-center text-[${mainIconColor}] hover:text-[${sndIconColor}] ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0 relative`}
+                        className={`border-2 border-sndIconColor hover:border-none hover:bg-mainIconColor w-[35px] h-[35px] rounded-full flex justify-center items-center text-mainIconColor hover:text-sndIconColor ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0 relative`}
                         title="Cart"
                     >
                         <FaShoppingCart size={21} />
                         <div className={`absolute -top-2 left-4 w-[20px] flex items-center justify-center h-[20px] bg-[red] rounded-full`}>
-                            <span className={`text-[${sndIconColor}] font-semibold`}>{'0'}</span>
+                            <span className={`text-sndIconColor font-semibold`}>{'0'}</span>
                         </div>
                     </div>
                 </Link>
                 <Link to="" className='xl:hidden'>
                     <div
-                        className={`border-2 border-[${sndIconColor}] hover:border-none hover:bg-[${mainIconColor}] w-[35px] h-[35px] rounded-full flex justify-center items-center text-[${mainIconColor}] hover:text-[${sndIconColor}] ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0 relative`}
+                        className={`border-2 border-sndIconColor hover:border-none hover:bg-mainIconColor w-[35px] h-[35px] rounded-full flex justify-center items-center text-mainIconColor hover:text-sndIconColor ease-in-out duration-100 cursor-pointer mr-3 xl:mr-0 relative`}
                         title='Favorite items'
                     >
                         <MdOutlineFavorite size={24} />
                         <div className={`absolute -top-2 left-4 w-[20px] flex items-center justify-center h-[20px] bg-[red] rounded-full`}>
-                            <span className={`text-[${sndIconColor}] font-semibold`}>{'0'}</span>
+                            <span className={`text-sndIconColor font-semibold`}>{'0'}</span>
                         </div>
                     </div>
                 </Link>
             </div>
             <div className=''>
                 <Link to="">
-                    <div className={`flex h-[35px] border-2 w-[100px] justify-center items-center rounded-full text-[${mainIconColor}] hover:bg-[${mainIconColor}] border-[${sndIconColor}] hover:text-[${sndIconColor}] hover:border-none`}>
+                    <div className={`flex h-[35px] border-2 w-[100px] justify-center items-center rounded-full text-mainIconColor hover:bg-mainIconColor border-sndIconColor hover:text-sndIconColor hover:border-none`}>
                         <div className='flex'>
-                            <FaUser size={20} className='mt-[1.5px]' />
-                            <span className={`ml-2 text-[${sndIconColor}]`}>Login</span>
+                            <FaUser size={20} className='mt-1.5px' />
+                            <span className={`ml-2 text-sndIconColor`}>Login</span>
                         </div>
                     </div>
                 </Link> 
--- Frontend/src/utils/colors.js
+++
@@ -1,4 +0,0 @@
-export const mainIconColor = "#2db12d";
-export const sndIconColor = "#c9c9c9";
-export const topBarTextColor = "#c9c9c9";
-export const topBarBgColor = "#202124"
