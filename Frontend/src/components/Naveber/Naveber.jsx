1e581dd40267112a21ec178dfdb2f73b38335684
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-16 20:13:46 +0600

    NaveBer created

--- Frontend/src/components/Naveber/Naveber.jsx
+++ Frontend/src/components/Naveber/Naveber.jsx
@@ -1,29 +1,55 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TopBar from './TopBar.Jsx';
+import Navigato from '../Navigator/Navigato';
+import { FaBars } from "react-icons/fa";
+import SideBarForMobile from './SideBarForMobile';
 
 const Naveber = () => {
-
+  const [items,setBar] = useState(false);
   return (
     <div className='w-full h-auto'>
       {
         window.location.href === "http://localhost:5173/" ? <TopBar /> : ""
       }
+      <SideBarForMobile on={items} setBar={setBar} />
          {/* This is the main navebar */}
-       <nav className='w-full bg-[#111111] h-[100px] z-50'>
-        <div className='max-w-[1280px] mx-auto px-6 sm:px-8 md:px-10  xl:px-0 duration-150 ease-in-out flex justify-between items-center'>
-          <div className=''>
-            <Link to="/">
-              <img src="/Image/Main_Logo.png" height={120} width={120} className='absolute top-10 z-50' alt="" />
+       <nav className='w-full bg-navebarBgColor h-[100px] z-50 relative'>
+        <div className='max-w-[1280px] mx-auto px-6 sm:px-8 md:px-10  xl:px-0 duration-150 ease-in-out flex justify-between items-center h-full'>
+          <div className='w-full h-full flex items-center'>
+            <Link to="/" className='w-[145px]'>
+              <img src="/logo.png" className='w-full h-full' alt="" />
             </Link>
           </div>
-          <div>
-            <Link to="">
-              <h2 className='text-[#c9c9c9] text-[16px]'>
+          <div className='xl:flex justify-between items-center w-[60%] h-full hidden'>
+            <Link to="/">
+              <h2 className='text-topBarTextColor text-[18px] hover:scale-105 duration-150 hover:text-[white]'>
                 Home
               </h2>
             </Link>
+            <Link to="/">
+              <h2 className='text-topBarTextColor text-[18px] hover:scale-105 duration-150 hover:text-[white]'>
+                Products
+              </h2>
+            </Link>
+            <h2 
+              className='text-topBarTextColor text-[18px] hover:scale-105 duration-150 hover:text-[white] cursor-pointer'
+            >
+              <Navigato />
+            </h2>
+            <Link to="/">
+              <h2 className='text-topBarTextColor text-[18px] hover:scale-105 duration-150 hover:text-[white]'>
+                Contact
+              </h2>
+            </Link>
+          </div>
+          <div
+            className='xl:hidden w-full h-full flex justify-end items-center'
+          >
+            <FaBars 
+              className='text-topBarTextColor text-[30px] active:scale-110'
+            />
           </div>
         </div>
        </nav>
