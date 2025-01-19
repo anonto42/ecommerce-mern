a16263391d164b65bf58c378ccd95a1659c90fb7
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-19 13:13:57 +0600

    add the main Suspense

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter, Route, Routes} from "react-router-dom"
+import ScreenBeforeMounted from './components/ScreenBeforeMounted/ScreenBeforeMounted'
 const Application = React.lazy( ()=>import('./utils/LazyConfig') )
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<h1>Loading....</h1>}>
+      <Suspense fallback={<ScreenBeforeMounted />}>
         <Application />
       </Suspense>
     </BrowserRouter>
