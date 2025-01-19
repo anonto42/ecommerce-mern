f28f5e795db8b5041003fb9334de8f8878a51703
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-19 13:05:09 +0600

    configd the suspence and all tnd test the suspensed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from './pages/Home/Home'
-import NavigationBar from './components/Naveber/NavigationBar'
-import Footer from './components/Footer/Footer'
+const Application = React.lazy( ()=>import('./utils/LazyConfig') )
 
 const App = () => {
   return (
     <BrowserRouter>
-    <NavigationBar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Application />
+      </Suspense>
     </BrowserRouter>
   )
 }
