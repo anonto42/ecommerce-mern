49b1f2f5abf630c7a0b636146bfe872586688e39
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-19 12:53:59 +0600

    extra import deleted

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Router, Routes} from "react-router-dom"
+import { BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/Home/Home'
 import NavigationBar from './components/Naveber/NavigationBar'
 import Footer from './components/Footer/Footer'
