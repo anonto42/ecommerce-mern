d47c7b3647dff3bf42adb865558c9d4f607e8244
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-19 12:51:56 +0600

    navigator addo solved

--- Frontend/src/components/Navigator/Navigato.jsx
+++ Frontend/src/components/Navigator/Navigato.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { IoMdArrowDropdown } from 'react-icons/io'
+import { IoMdArrowDropdown, IoMdArrowDropup, IoMdArrowUp } from 'react-icons/io'
 import ItemsOfNavigation from './ItemsOfNavigation';
 
 const Navigato = ({name="Catagorys",items=["T-shirt","Poto-shirt",'Shirt',"Pant"]}) => {
@@ -11,12 +11,13 @@ const Navigato = ({name="Catagorys",items=["T-shirt","Poto-shirt",'Shirt',"Pant"
         onMouseLeave={()=>setYes(false)} 
         onClick={()=>setYes(!yes)}>
         <h2 className=''>{name}</h2>
-        <IoMdArrowDropdown className='mt-[4px] text-[25px]' />
-
-        {/* {
-            yes? <ItemsOfNavigation item={items} on={yes} /> : <></>
-        } */}
-
+        {
+          yes? (
+            <IoMdArrowDropup className='mt-[4px] text-[25px]' />
+          ):(
+            <IoMdArrowDropdown className='mt-[4px] text-[25px]' />
+          )
+        }
         <ItemsOfNavigation item={items} on={yes} />
 
     </div>
