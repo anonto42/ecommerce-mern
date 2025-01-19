8d029524a92053b0dbdcb382e7f122159ddb1518
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-19 17:32:13 +0600

    modifide the colors config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -8,20 +8,22 @@ export default {
     extend: {
       screens:{
         "xl":"1450px"
+      },
+        colors:{
+        bodyColor: "#09090B",
+        mainBg : "#202124",
+        topBarBgColor : "#202124",
+        sndIconColor : "#c9c9c9",
+        mainIconColor : "#2db12d",
+        topBarTextColor : "#c9c9c9",
+        sndTextColor: "#afafaf",
+        navebarBgColor : "#111111",
+        navigatorBg : "#c9c9c9",
+        navigatorTextColor : "#202124",
+        textDarkColor : "#0c0c0c",
+        btn:"#2db12d",
+        inputOnAuthRing:"#cecece"
       }
-    },
-    colors:{
-      bodyColor: "#09090B",
-      mainBg : "#202124",
-      topBarBgColor : "#202124",
-      sndIconColor : "#c9c9c9",
-      mainIconColor : "#2db12d",
-      topBarTextColor : "#c9c9c9",
-      sndTextColor: "#afafaf",
-      navebarBgColor : "#111111",
-      navigatorBg : "#c9c9c9",
-      navigatorTextColor : "#202124",
-      textDarkColor : "#0c0c0c"
     }
   },
   plugins: [],
