91dae4a8a200bd2f29702a82ab77952b60104ac0
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-20 16:55:20 +0600

    now this is only for route manage ment

--- Frontend/src/utils/LazyConfig.jsx
+++ Frontend/src/utils/LazyConfig.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Auth from '../pages/Auth/Auth'
-import NavigationBar from '../components/Naveber/NavigationBar'
-import Footer from '../components/Footer/Footer'
 
 const LazyConfig = () => {
   return (
     <>
-      <NavigationBar />
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/auth' element={<Auth/>} />
-        </Routes>
-      <Footer />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/auth' element={<Auth/>} />
+      </Routes>
     </>
   )
 }
