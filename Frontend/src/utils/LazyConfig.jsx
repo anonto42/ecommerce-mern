039a1a0b3c1d346603b4d20108e27974df0b172c
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-20 17:15:05 +0600

    nothink was hapand

--- Frontend/src/utils/LazyConfig.jsx
+++ Frontend/src/utils/LazyConfig.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Auth from '../pages/Auth/Auth'
+import NavigationBar from '../components/Naveber/NavigationBar'
+import Footer from '../components/Footer/Footer'
 
 const LazyConfig = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/auth' element={<Auth/>} />
-      </Routes>
+      <NavigationBar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/auth' element={<Auth/>} />
+        </Routes>
+      <Footer />
     </>
   )
 }
