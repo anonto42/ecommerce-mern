883d6f7164dd7fb53b41daf0c9aa96731cc20488
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-21 18:51:38 +0600

    updated the slider duration

--- Frontend/src/components/Slider/Slider.jsx
+++ Frontend/src/components/Slider/Slider.jsx
@@ -5,7 +5,7 @@ const Slider = ({ images }) => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000);
+        }, 5000);
         return () => clearInterval(interval);
     }, [images.length]);
     function leftSlite(){
