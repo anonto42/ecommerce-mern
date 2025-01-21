85ca7d87e3fcbac5a3c5087d12b6bc65903c5df5
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-21 16:43:49 +0600

    created the slider

--- Frontend/src/components/Hero/HeroSection.jsx
+++ Frontend/src/components/Hero/HeroSection.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Slider from '../Slider/Slider';
 
 const HeroSection = () => {
+
+  const images = [
+      "https://picsum.photos/id/1018/1000/600/",
+      "https://picsum.photos/id/1015/1000/600/",
+      "https://picsum.photos/id/1019/1000/600/",
+  ];
+
   return (
-    <div>HeroSection</div>
+    <div 
+       className='w-full h-[55svh] relative'
+    >
+      <Slider images={images} />
+
+      <div
+        className='w-full h-full absolute top-0'
+      >
+        
+      </div>
+        
+    </div>
   )
 }
 
--- Frontend/src/components/Slider/Slider.jsx
+++ Frontend/src/components/Slider/Slider.jsx
@@ -19,7 +19,7 @@ const Slider = ({ images }) => {
     return (
         <div className="relative w-full h-full overflow-hidden">
             <div 
-                className='w-full h-full flex absolute z-50'
+                className='w-full h-full flex absolute z-20'
             >
                 <div
                     onClick={()=>leftSlite()}
