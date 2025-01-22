59947b422c1527bb2929de01e2862d6499c8bf48
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-22 19:25:32 +0600

    image import improved

--- Frontend/src/components/Hero/HeroSection.jsx
+++ Frontend/src/components/Hero/HeroSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Slider from '../Slider/Slider';
-import Image1 from "../../../public/hero.jpg";
-import Image2 from "../../../public/hero2.jpg";
-import Image3 from "../../../public/hero3.jpg";
+import Image1 from "/hero.jpg";
+import Image2 from "/hero2.jpg";
+import Image3 from "/hero3.jpg";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react"
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
