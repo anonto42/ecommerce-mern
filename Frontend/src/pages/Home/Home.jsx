8c2a631d5992040d15e144b2180a9042e1c4ec00
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-22 10:09:56 +0600

    Best Selling produts component added

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../../components/Hero/HeroSection'
+import BestSellingProducts from '../../components/Highlights/BestSellingProducts'
 
 const Home = () => {
   return (
     <div className='min-h-svh w-full'>
       <HeroSection />
+      <BestSellingProducts />
     </div>
   )
 }
