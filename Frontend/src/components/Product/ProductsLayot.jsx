a94d904dab59d21eba6da7f10ceaa56913a795da
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-23 14:29:57 +0600

    layout for the all products

---
+++ Frontend/src/components/Product/ProductsLayot.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductsLayot = ({allProducts}) => {
+  return (
+    <div 
+        className='w-full h-full'>
+        <div 
+            className='max-w-[1400px] mx-auto px-6 sm:px-8 md:px-10 xl:px-0 h-full pt-4 sm:pt-8 md:pt-10'
+        ></div>
+    </div>
+  )
+}
+
+export default ProductsLayot
