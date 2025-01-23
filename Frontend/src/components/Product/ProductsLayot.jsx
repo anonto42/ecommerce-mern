927d1bb5a8791a03b23bf779844e00c3041c33f4
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-23 20:09:07 +0600

    updated

--- Frontend/src/components/Product/ProductsLayot.jsx
+++ Frontend/src/components/Product/ProductsLayot.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import ShowcaseSlider from '../Slider/ShowcaseSlider'
 
-const ProductsLayot = ({allProducts}) => {
+const ProductsLayot = ({allProducts,showCaseName}) => {
   return (
     <div 
-        className='w-full h-full'>
-        <div 
-            className='max-w-[1400px] mx-auto px-6 sm:px-8 md:px-10 xl:px-0 h-full pt-4 sm:pt-8 md:pt-10'
-        ></div>
+        className='w-full h-full'
+    >
+        <div
+        className='max-w-[1400px] mx-auto px-6 sm:px-8 md:px-10 xl:px-0 h-full pt-4 sm:pt-8 md:pt-10'
+      >
+        <h1 
+            className='text-topBarTextColor text-2xl sm:text-3xl md:text-4xl font-semibold font-serif'>
+                {
+                    showCaseName
+                }
+        </h1>
+      </div>
+      <div
+        className='max-w-[1400px] mx-auto h-[300px]'
+      >
+        <ShowcaseSlider cart={allProducts} />
+      </div>
     </div>
   )
 }
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import HeroSection from '../../components/Hero/HeroSection'
 import ProductShowCase from '../../components/Highlights/ProductShowCase'
 import { useSelector } from 'react-redux'
+import ProductsLayot from '../../components/Product/ProductsLayot'
 
 const Home = () => {
-  const { bestSellingProducts } = useSelector( data => data.applicationData.appData )
+  const { bestSellingProducts , specialOffersDiscounts , products } = useSelector( data => data.applicationData.appData )
   const data = useSelector( data => data.applicationData.appData )
   // console.log(data)
   return (
     <div className='min-h-svh w-full'>
       <HeroSection />
-      <ProductShowCase showCaseName={"Best Selling Products"} bestSellingProducts={bestSellingProducts} />
+      <ProductShowCase key={1} showCaseName={"Special Offers"} sellingProducts={specialOffersDiscounts} />
+      <ProductShowCase key={2} showCaseName={"Best Selling Products"} sellingProducts={bestSellingProducts} />
+      <ProductsLayot showCaseName={"All hot item's"} allProducts={products} />
     </div>
   )
 }
