fbeea29bca845d81e65f9b5cac473d5bd07e2de9
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-24 22:44:21 +0600

    no page created

---
+++ Frontend/src/pages/Nopage/Nopage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaHome } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const Nopage = () => {
+  return (
+    <div
+        className='w-full h-[50svh] flex justify-center items-center text-topBarTextColor'
+    >
+        <div 
+            className='text-center'
+        >
+            <h1 
+                className='text-center text-2xl my-4'
+            >
+                <strong>
+                    Sorry, this page isn't available.
+                </strong>
+                    <br />
+                <small
+                    className='font-light italic'
+                >
+                    Click here to return to the homepage.
+                </small>
+            </h1>
+            <Link to={"/"}>
+                <button 
+                    className='text-4xl active:scale-105 text-mainIconColor'
+                >
+                    <FaHome />
+                </button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Nopage
