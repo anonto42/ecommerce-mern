7fe346afd735aca688e072bab33d77c94b9c5f46
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-24 21:47:57 +0600

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/auth' element={<Auth/>} />
+          <Route path='*' element={<ScreenBeforeMounted />} />
         </Routes>
       <Footer />
       <ToastContainer />
--- Frontend/src/components/ScreenBeforeMounted/ScreenBeforeMounted.jsx
+++ Frontend/src/components/ScreenBeforeMounted/ScreenBeforeMounted.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Slider from '../Slider/Slider'
+import { RotatingLines } from 'react-loader-spinner'
 
 const ScreenBeforeMounted = () => {
   return (
-    <div>ScreenBeforeMounted</div>
+    <div
+      className='w-full min-h-svh bg-[#a5a5a5] relative'
+    >
+      <nav
+      >
+        <div
+          className='w-full h-[70px] bg-[#292929]'
+        ></div>
+        <div
+          className='w-full h-[90px] bg-[#5a5a5a] shadow-xl shadow-[#00000080] z-20 flex justify-center items-center'
+        >
+          <RotatingLines
+          width={55}
+          />
+        </div>
+      </nav>
+      <div
+        className='w-full h-[55vh] bg-[#ffffffa2] flex justify-center items-center'
+      >
+        <RotatingLines />
+      </div>
+    </div>
   )
 }
 
