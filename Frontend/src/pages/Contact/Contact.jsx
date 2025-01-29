85b2f4f05a8c422fb0e60c60322d4e51f9f2dd17
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-29 13:29:44 +0600

    size problem solved

--- Frontend/src/pages/Contact/Contact.jsx
+++ Frontend/src/pages/Contact/Contact.jsx
@@ -8,32 +8,38 @@ const Contact = () => {
         <div
             className='max-w-[1400px] mx-auto px-6 sm:px-8 md:px-10 xl:px-0'
         >
+
             <div
-                className='w-[500px] h-[150px] mx-auto text-center pt-4'
+                className='max-w-[500px] h-[150px] mx-auto text-center pt-6 text-[18px]'
             >
                 <h1
-                    className='text-2xl font-serif font-semibold mb-4'
+                    className='md:text-2xl font-serif font-semibold mb-4'
                 >Our whatsapp number : 
                     <small
-                        className='text-lg font-light font-mono'
+                        className='md:text-lg font-light font-mono'
                     >
                         {" "}+91 9999999999
                     </small>
                 </h1>
 
                 <h1
-                    className='text-2xl font-serif font-semibold mb-4'
+                    className='md:text-2xl font-serif font-semibold mb-4'
                 >Our email address : 
                     <small
-                        className='text-lg font-light font-mono'
+                        className='md:text-lg font-light font-mono'
                     >
                        {" "} example@example.com
                     </small>
                 </h1>
 
             </div>
+
+
+
+
+
             <div
-                className='max-w-[800px] mx-auto min-h-[500px] mt-6 relative'
+                className='max-w-[800px] mx-auto min-h-[500px] mt-6 md:mt-0'
             >
                 <form action="" className=''>
                     <h1
@@ -101,9 +107,14 @@ const Contact = () => {
                         />
                     </div>
 
-                    <button
-                        className='absolute bottom-2 right-2 rounded-lg md:w-[130px] md:h-[50px] w-[100px] h-[40px] bg-mainIconColor text-textDarkColor font-serif font-semibold text-lg md:text-xl active:scale-105'
-                    >Send</button>
+                    <div
+                        className='w-full h-[50px] sm:h-[60px] md:h-[70px] relative mb-4'
+                    >
+                        <button
+                            className='absolute bottom-2 right-2 rounded-lg md:w-[130px] md:h-[50px] w-[100px] h-[40px] bg-mainIconColor text-textDarkColor font-serif font-semibold text-lg md:text-xl active:scale-105'
+                        >Send</button>
+                    </div>
+
                 </form>
             </div>
         </div>
