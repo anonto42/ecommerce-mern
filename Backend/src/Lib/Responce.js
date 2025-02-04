2c7d9ae0e77380b546e7bc197c5815382d89c54a
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-04 20:03:33 +0600

    tend the responce as json

--- Backend/src/Lib/Responce.js
+++ Backend/src/Lib/Responce.js
@@ -1,12 +1,11 @@
 class Responce {
     constructor( message , data = null , success ){
-        this.data = data;
-        this.success = success;
         this.message = message;
+        this.success = success;
+        this.data = data;
     }
 
     static success( message , data , success ){
-        
         return new Responce( message , data , success );
     }
 
