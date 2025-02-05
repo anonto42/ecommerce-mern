4f0c26e1d2095fea3504503977bc0954a9d614f6
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-05 18:45:49 +0600

    get user data success fully

--- Frontend/src/utils/Promis.js
+++ Frontend/src/utils/Promis.js
@@ -1,10 +1,44 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAppData, setUserData } from "../Redux/slices/dataFromServer";
+import axios from "axios";
 
 
 
 
-const dispatch = useDispatch();
+
+const Promis = () => {
+    const dispatch = useDispatch();
+    const [user,setUser] = useState(undefined);
+    const { userData , appdata } = useSelector( event => event.applicationData)
+
+    
+    useEffect(()=>{
+        
+        ;(async ()=> {
+            if (user) return ;
+            const { data } = await axios.get(`${import.meta.env.VITE_REACT_SERVER_API}/user/user`,{withCredentials:true});
+            setUser(data.data);
+            dispatch(
+                setUserData(data.data)
+            )
+        })()
+    
+    
+    },[user])
 
 
-useEffect(()=>{},[])
+
+
+
+
+
+
+
+
+
+
+
+    return undefined
+}
+export default Promis;
