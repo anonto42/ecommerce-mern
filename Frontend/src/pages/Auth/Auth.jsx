ac66a0aca040517e1193df639b2d8a63c500c8eb
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-05 16:26:45 +0600

    login functiona added

--- Frontend/src/pages/Auth/Auth.jsx
+++ Frontend/src/pages/Auth/Auth.jsx
@@ -1,17 +1,104 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
+import { Hourglass } from 'react-loader-spinner';
+import { toast } from 'react-toastify';
+import { headerConfigForPost } from '../../utils/helper';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
   const [auth,setAuth] = useState(true);
   const [forgot,setForgot] = useState(false);
+  const [load,setLoading] = useState(false);
+  const server = import.meta.env.VITE_REACT_SERVER_API
+
+  const formData = new FormData();
+
+  // Login funcion
+  const login = async (e) => {
+    try {
+      setLoading(true);
+      e.preventDefault();
+
+      formData.append('email', email);
+      formData.append('password', password);
+
+      const data =  await axios.post(`${server}/user/login`, formData , headerConfigForPost );
+console.log(data)
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+
+      toast.success("Logged In Successfully");
+      setTimeout(() => {}, 3000);
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+      toast.error("Couldn't login")
+    }
+  }
+
+  // Register funcion
+  const registre = async (e) => {
+    try {
+      setLoading(true);
+      e.preventDefault();
+
+      if( password != confirmPassword ) return toast.error("Please check the confirm password");
+
+      formData.append('email', email);
+      formData.append('password', password);
+
+      const respons = await axios.post(`${server}/user/login`, formData , headerConfigForPost );
+
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+
+      toast.success("Logged In Successfully");
+      setTimeout(() => {}, 3000);
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+      toast.error("Couldn't login")
+    }
+  }
+
+
+
   return (
-      <div className="flex items-center justify-center min-h-[70svh]">
+      <div className="flex items-center justify-center min-h-[70svh] relative">
+        {
+          load?(
+            <div className='absolute h-full w-full bg-[#c7c0c033] flex justify-center items-center'>
+              <Hourglass
+                visible={true}
+                height="60"
+                width="60"
+                ariaLabel="hourglass-loading"
+                wrapperClass=""
+                colors={['#2db12d', '#72a1ed']}
+              />
+            </div>
+          ):(<></>)
+        }
       <div className="bg-[white] shadow-lg rounded-lg p-8 max-w-xs md:max-w-md w-full">
           {
             forgot ? (
             <div id="forgot-password-form">
               <h2 className="text-xl md:text-2xl font-bold text-center text-gray-800 mb-6">Forgot Password</h2>
               <form>
-                  <input type="email" placeholder="Email" className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   required />
+                  <input 
+                    type="email" placeholder="Email" 
+                    className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   
+                    required
+                    value={email}
+                    onChange={ e => setEmail(e.target.value )}
+                  />
                   <button type="submit" className="w-full bg-btn text-white py-2 md:py-3 rounded-lg font-semibold hover:bg-[#1f861b]  transition duration-200">Reset   Password</button>
                   <p className="text-center text-gray-600 mt-6">
                       Remember your password? 
@@ -27,10 +114,24 @@ const Auth = () => {
             ):(auth === true ?(
               <div id="login-form" className={forgot?"hidden":""}>
                 <h2 className="text-xl md:text-2xl font-bold text-center text-gray-800 mb-6">Log In</h2>
-                <form>
-                  <input type="email" placeholder="Email" className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   required />
-                  <input type="password" placeholder="Password" className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" required />
-                  <button type="submit" className="w-full bg-btn text-white py-2 md:py-3 rounded-lg font-semibold hover:bg-[#1f861b] transition duration-200">Log In</  button>
+                <form action='POST'>
+                  <input 
+                    type="email" placeholder="Email" 
+                    className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   
+                    required
+                    value={email}
+                    onChange={ e => setEmail(e.target.value )} 
+                  />
+                  <input 
+                    type="password" placeholder="Password" 
+                    className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" 
+                    required
+                    value={password}
+                    onChange={ e => setPassword(e.target.value )} 
+                  />
+                  <button
+                    onClick={login}
+                    className="w-full bg-btn text-white py-2 md:py-3 rounded-lg font-semibold hover:bg-[#1f861b] transition duration-200">Log In</button>
                   <p className="text-center text-gray-500 mt-6">
                       <a href="#" className="text-blue-600 hover:underline" onClick={()=>setForgot(true)}>Forgot Password?</a>
                   </p>
@@ -44,10 +145,34 @@ const Auth = () => {
               <div id="signup-form" className={forgot?"hidden":""} >
                 <h2 className="text-xl md:text-2xl font-bold text-center text-gray-800 mb-6">Sign Up</h2>
                 <form>
-                  <input type="text" placeholder="Name" className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   required />
-                  <input type="email" placeholder="Email" className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   required />
-                  <input type="password" placeholder="Password" className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" required />
-                  <input type="password" placeholder="Confirm Password" className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" required />
+                  <input 
+                    type="text" placeholder="Name" 
+                    className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   
+                    required
+                    value={name}
+                    onChange={ e => setName(e.target.value )} 
+                  />
+                  <input 
+                    type="email" placeholder="Email" 
+                    className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-inputOnAuthRing"   
+                    required
+                    value={email}
+                    onChange={ e => setEmail(e.target.value )}
+                   />
+                  <input 
+                    type="password" placeholder="Password" 
+                    className="w-full mb-4 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" 
+                    required
+                    value={password}
+                    onChange={ e => setPassword(e.target.value )} 
+                  />
+                  <input 
+                    type="password" placeholder="Confirm Password" 
+                    className="w-full mb-6 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2  focus:ring-inputOnAuthRing" 
+                    required
+                    value={confirmPassword}
+                    onChange={ e => setConfirmPassword(e.target.value )}
+                    />
                   <button type="submit" className="w-full bg-btn text-white py-2 md:py-3 rounded-lg font-semibold hover:bg-[#1f861b]  transition duration-200">Sign Up</ button>
                   <p className="text-center text-gray-600 mt-6">
                       Already have an account? 
---
+++ Frontend/src/utils/helper.js
@@ -0,0 +1,9 @@
+const headerConfigForPost = {
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    },
+    withCredentials: true
+};
+
+
+export { headerConfigForPost }
