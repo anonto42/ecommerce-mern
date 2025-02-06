2c26a144925a3a1639206729bd212206113cf91f
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-07 00:02:47 +0600

    added the multer for upload the files

--- Backend/src/Middleware/Multer.js
+++ Backend/src/Middleware/Multer.js
@@ -1,1 +1,16 @@
 import multer from 'multer'
+
+// Create a new Date object to get current date and time for file name
+const now = new Date()
+const DD_MM_YYYY = `${now.getDate().toString().padStart(2, '0')}_${(now.getMonth() + 1).toString().padStart(2, '0')}_${now.getFullYear()}`
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/Images/')
+    },
+    filename: function (req, file, cb) {
+        cb(null,`${DD_MM_YYYY}-${file.originalname}`)
+    }
+})
+
+export const uploader = multer({ storage: storage })
