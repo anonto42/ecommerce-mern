be54d469beb300cbfe19ac32a16a6a2a73a2dac2
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-09 20:14:38 +0600

    created user find and block and delete

--- Frontend/src/components/Admin/UsersComponent.jsx
+++ Frontend/src/components/Admin/UsersComponent.jsx
@@ -1,13 +1,129 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { Hourglass } from 'react-loader-spinner';
 
 const UsersComponent = () => {
+    const [loading,setLoading] = useState(false);
+    const { totalUsers } = useSelector( data => data.applicationData.adminData);
+    const [email, setSearchInput] = useState('');
+    const [user,setUser] = useState('')
+
+    console.log(user)
+
+    const SerchUser = async () => {
+      try {
+        setLoading(true);
+        if (email == '') {
+          return toast.warn("Please enter a email address of user!");
+        }
+    
+        const response = await axios.post(`${import.meta.env.VITE_REACT_SERVER_API}/admin/user`, {email}
+          , // Send email in request body
+          { withCredentials: true,
+            headers: { 
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*' // This is for CORS
+            }
+           } // Config object
+        );
+        toast.success("User found successfully!");
+        setUser(response.data.data); // Assuming you have a setUser state
+      } catch (error) {
+        toast.error("User not exist!");
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+      async function block () {
+        try {
+          setLoading(true);
+          if (email == '') {
+            return toast.warn("Please enter a email address of user!");
+          }
+      
+          const response = await axios.put(`${import.meta.env.VITE_REACT_SERVER_API}/admin/user`, {email,block:true}
+            , // Send email in request body
+            { withCredentials: true,
+              headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' // This is for CORS
+              }
+            } // Config object
+          );
+          toast.success("User Blocked!");
+          setUser(response.data.data); // Assuming you have a setUser state
+        } catch (error) {
+          toast.error("Something went wrong!");
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      async function unBlock () {
+        try {
+          setLoading(true);
+          if (email == '') {
+            return toast.warn("Please enter a email address of user!");
+          }
+      
+          const response = await axios.put(`${import.meta.env.VITE_REACT_SERVER_API}/admin/user`, {email,block:false}
+            , // Send email in request body
+            { withCredentials: true,
+              headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' // This is for CORS
+              }
+            } // Config object
+          );
+          toast.success("User Unblocked!");
+          setUser(response.data.data); // Assuming you have a setUser state
+        } catch (error) {
+          toast.error("Something went wrong!");
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+    const deleteUser = async () => {
+        try {
+          setLoading(true);
+          if (email == '') {
+            return toast.warn("Please enter a email address of user!");
+          }
+      
+          const response = await axios.delete(`${import.meta.env.VITE_REACT_SERVER_API}/admin/user`, {email}
+            , // Send email in request bodyimport { Hourglass } from 'react-loader-spinner';
+
+            { withCredentials: true,
+              headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' // This is for CORS
+              }
+            } // Config object
+          );
+          toast.success("User Deleted.");
+          setUser(response.data.data); // Assuming you have a setUser state
+        } catch (error) {
+          toast.error("Something went wrong!");
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+    }
+    
+
   return (
     <div
       className='w-full h-full px-4'
     >
       <h1
         className='text-2xl underline py-4 text-topBarTextColor font-semibold'
-      >Total user's:</h1>
+      >Total user's: ({totalUsers.length})</h1>
 
       <div
         className='w-full overflow-auto h-[40vh] border border-topBarTextColor rounded-lg'
@@ -31,70 +147,27 @@ const UsersComponent = () => {
         </div>
 
           {/* Main user accounts */}
-        <div
-          className='w-[1300px] h-[40px] border-b border-[#80808079] text-topBarTextColor items-center flex px-4 justify-between'
-        >
-          <h1
-            className='italic w-[325px] font-mono'
-          >Sohidul islam Anonto</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >anontom90@gmail.com</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >0199901010</h1>
-          <h1
-            className='italic w-[300px] font-mono flex justify-start'
-          >6</h1>
-        </div>
-        <div
-          className='w-[1300px] h-[40px] border-b border-[#80808079] text-topBarTextColor items-center flex px-4 justify-between'
-        >
-          <h1
-            className='italic w-[325px] font-mono'
-          >Sohidul islam Anonto</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >anontom90@gmail.com</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >0199901010</h1>
-          <h1
-            className='italic w-[300px] font-mono flex justify-start'
-          >6</h1>
-        </div>
-        <div
-          className='w-[1300px] h-[40px] border-b border-[#80808079] text-topBarTextColor items-center flex px-4 justify-between'
-        >
-          <h1
-            className='italic w-[325px] font-mono'
-          >Sohidul islam Anonto</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >anontom90@gmail.com</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >0199901010</h1>
-          <h1
-            className='italic w-[300px] font-mono flex justify-start'
-          >6</h1>
-        </div>
-        <div
-          className='w-[1300px] h-[40px] border-b border-[#80808079] text-topBarTextColor items-center flex px-4 justify-between'
-        >
-          <h1
-            className='italic w-[325px] font-mono'
-          >Sohidul islam Anonto</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >anontom90@gmail.com</h1>
-          <h1
-            className='italic w-[325px] font-mono flex justify-start'
-          >0199901010</h1>
-          <h1
-            className='italic w-[300px] font-mono flex justify-start'
-          >6</h1>
-        </div>
+          {
+            totalUsers.map((user,index) => (
+              <div
+                key={index}
+                className='w-[1300px] h-[40px] border-b border-[#80808079] text-topBarTextColor items-center flex px-4 justify-between'
+              >
+                <h1
+                  className='italic w-[325px] font-mono'
+                >{user.name}</h1>
+                <h1
+                  className='italic w-[325px] font-mono flex justify-start'
+                >{user.email}</h1>
+                <h1
+                  className='italic w-[325px] font-mono flex justify-start'
+                >{user.number}</h1>
+                <h1
+                  className='italic w-[300px] font-mono flex justify-start'
+                >{user.orders.length}</h1>
+              </div>
+            ))
+          }
 
       </div>
 
@@ -106,43 +179,80 @@ const UsersComponent = () => {
       <div
         className='w-full h-[80px]'
       >
-        <form action="">
-          <div
+        <div
             className='md:w-[350px] w-[250px] h-[50px] mt-4 rounded-lg overflow-hidden font-serif'
           >
             <input 
+              value={email}
+              onChange={ e => setSearchInput( e.target.value ) }
               type="text" 
               className='h-full outline-none px-4 text-lg w-[80%] text-textDarkColor'
               placeholder='Search user by email'
               />
             <button
+              onClick={() => SerchUser()}
               className='w-[20%] h-[53px] bg-blue-400 text-white hover:bg-blue-500'
             >Search</button>
-          </div>
-        </form>
+        </div>
       </div>
 
       {/* Serch user info */}
       
         <div
-          className='w-full h-[340px] text-topBarTextColor'
+          className='w-full min-h-[240px] pb-4 text-topBarTextColor relative'
         >
+          {
+          loading?(
+            <div className='absolute h-full w-full flex justify-center items-center'>
+              <Hourglass
+                visible={true}
+                height="60"
+                width="60"
+                ariaLabel="hourglass-loading"
+                wrapperClass=""
+                colors={['#2db12d', '#72a1ed']}
+              />
+            </div>
+          ):(<></>)
+        }
           {/* Dufalt value */}
           <h1
             className='text-2xl underline py-4 font-semibold'
           >User info:</h1>
 
-          <h3>Name: <small>{"This is the name of the"}</small></h3>
-          <h3>Email: <small>{"This is the name of the"}</small></h3>
-          <h3>Number: <small>{"This is the name of the"}</small></h3>
-          <h3>Address: <small>{"This is the name of the"}</small></h3>
           <div>
-            <button
-              className='w-[110px] h-[40px] hover:bg-red-800 cursor-pointer active:scale-95 bg-red-700 mt-4 rounded-md text-lg font-semibold font-serif'
-            >Block</button>
-            <button
-            className='w-[110px] h-[40px] hover:bg-pink-800 cursor-pointer active:scale-95 bg-pink-700 mt-4 rounded-md text-lg font-semibold font-serif ml-2'
-          >Delete</button>
+            <h3>Name: <small>{user.name}</small></h3>
+            <h3>Email: <small>{user.email}</small></h3>
+            <h3>Number: <small>{user.number}</small></h3>
+            <div
+              className='flex'
+            >Address:(
+              <p className='text-sm font-mono'>{user.city}</p>) ; ( 
+              <p className='text-sm font-mono'>{user.thana}</p> ) ; (
+              <p className='text-sm font-mono'>{user.location}</p> )
+            </div>
+            <h3>Status: <small>{
+                user.isBlocked == "" ?
+                  user.isBlocked != true ? "Unblock":"Block"
+                : "....."}</small></h3>
+            <div
+              className=''
+            >
+              <button
+                onClick={()=>{
+                  user.isBlocked == true ? unBlock():block()
+                }}
+                className='w-[110px] h-[40px] hover:bg-red-800 cursor-pointer active:scale-95 bg-red-700 mt-4 rounded-md text-lg font-semibold font-serif'
+              >
+                {
+                  user.isBlocked == true ? "Unblock":"Block"
+                }
+              </button>
+              <button
+              onClick={()=>deleteUser()}
+              className='w-[110px] h-[40px] hover:bg-pink-800 cursor-pointer active:scale-95 bg-pink-700 mt-4 rounded-md text-lg font-semibold font-serif ml-2'
+            >Delete</button>
+          </div>
           </div>
 
         </div>
