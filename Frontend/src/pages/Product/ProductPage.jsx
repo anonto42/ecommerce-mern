c4e6aa01ea3ac8937997f6798d4c51a134568c49
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-13 23:22:19 +0600

    added the show image and outer geting oparatons

--- Frontend/src/pages/Product/ProductPage.jsx
+++ Frontend/src/pages/Product/ProductPage.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from 'react'
-
-import image1 from "/hero.jpg"
-import image2 from "/hero2.jpg"
-import image3 from "/hero3.jpg"
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from "react-router-dom"
 import { useSelector } from 'react-redux'
-import { MdStar } from 'react-icons/md'
+import { MdStar, MdStarHalf, MdStarOutline, MdStarRate } from 'react-icons/md'
+import axios from 'axios';
+import { RotatingLines } from 'react-loader-spinner';
 
 const ProductPage = () => {
-  const {products} = useSelector( e => e.applicationData.appData )
-  const img = [
-      image1, image2, image3
-    ]
-  console.log(products)
-  const [image,setImage] = useState(img[0])
+  let img = []
+  const [productData , setProductData] = useState({})
+
   const [imgLoaction,setImageLocation] = useState(1)
   const [load,setLoad] = useState(false)
+  const [searchParams] = useSearchParams();
+  const productID = searchParams.get("id")
+
+  useEffect(() => {
+    if (!productID) return window.location.href = '/'
+
+    ;(async () => {
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_REACT_SERVER_API}/user/product`,
+          { id: productID }
+        )
+        // .then( rs => console.log(rs.data.data))
+        .then( rs =>( setProductData(rs?.data?.data), setImage(rs?.data?.data?.images)))
+        .catch( err => console.log(err))
+      } catch (error) {
+        console.log("Error fetching product:", error);
+      }
+    })();
+    
+  },[]);
+  const avalableSizes = productData?.size?.split(" ");
+  const [selectedSize, setSelectedSize] = useState("");
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+      productData?.images?.length > 0 ? setImage(productData.images[0]) : ""
+  }, [productData]);
+
+  
   return (
     <div
       className='w-full min-h-svh'
@@ -23,76 +49,62 @@ const ProductPage = () => {
         className='lg:max-w-[1400px] w-full mx-auto h-full lg:flex lg:mt-16'
       >
         <div
-          className='w-full h-auto px-4 pt-4 relative'
+          className='w-full h-auto px-4 pt-4'
         >
-          <img 
-            src={image} 
-            alt="#" 
-            loading='lazy'
-            onLoad={()=>setLoad(true)}
+
+          <div
             className='w-[350px] md:w-[450px] md:h-[550px] mx-auto h-[450px] border-2 border-mainIconColor rounded-md overflow-hidden'
-          />
-          {
-            !load?(
+          >
+            { !load?
               <div 
-                className='w-[320px] absolute z-20 md:w-[450px] md:h-[550px] mx-auto h-[350px] bg-slate-100 border-2 border-mainIconColor rounded-md overflow-hidden'
-              ></div>
-            ):(<></>)
-          }
+                className={`bg-slate-100 w-full h-full duration-100 ease-linear rounded-md  flex justify-center items-center`}
+              >
+                <RotatingLines color="#16b916" width='60px' />
+              </div> : <></>
+            }
+
+            <img 
+              src={image} 
+              alt="#" 
+              loading='lazy'
+              className='w-full h-full'
+            />
+            
+          </div>
+          
           <div
             className='w-full pt-4 flex justify-center items-center'
           >
             <div
               className='flex'
             >
-              <div
-                onClick={()=>{setImage(img[0]);setImageLocation(1)}}
-                className={imgLoaction===1?'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-mainIconColor ml-2 cursor-pointer relative':'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-topBarTextColor ml-2 cursor-pointer relative'}
-              >
-                <img 
-                  src={img[0]} 
-                  alt="" 
-                  loading='lazy'
-                  className='w-full h-full'
-                />
-                {!load?(
-                    <div 
-                      className='w-full h-full bg-slate-100 absolute z-20'
-                    ></div>
-                    ):(<></>)}
-              </div>
-              <div
-                onClick={()=>{setImage(img[1]);setImageLocation(2)}}
-                className={imgLoaction===2?'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-mainIconColor ml-2 cursor-pointer relative':'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-topBarTextColor ml-2 cursor-pointer relative'}
-              >
-                <img 
-                  src={img[1]} 
-                  alt="" 
-                  loading='lazy'
-                  className='w-full h-full'
-                />
-                {!load?(
-                    <div 
-                      className='w-full h-full bg-slate-100 absolute z-20'
-                    ></div>
-                    ):(<></>)}
-              </div>
-              <div
-                onClick={()=>{setImage(img[2]);setImageLocation(3)}}
-                className={imgLoaction===3?'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-mainIconColor ml-2 cursor-pointer relative':'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-topBarTextColor ml-2 cursor-pointer relative'}
-              >
-                <img 
-                  src={img[2]} 
-                  alt="" 
-                  loading='lazy'
-                  className='w-full h-full'
-                />
-                {!load?(
-                    <div 
-                      className='w-full h-full bg-slate-100 absolute z-20'
-                    ></div>
-                    ):(<></>)}
-              </div>
+              {
+                productData?.images?.map(( item , index )=>{
+                  return(
+                    <div
+                      key={index}
+                      onClick={()=>{setImage(item);setImageLocation(index+1)}}
+                      className={ imgLoaction === (index+1) ?'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-mainIconColor ml-2 cursor-pointer relative':'w-[80px] h-[80px] md:w-[110px] md:h-[110px] overflow-hidden rounded-md border-2 border-topBarTextColor ml-2 cursor-pointer'}
+                    >
+                      {!load?
+                        <div 
+                        className={`bg-slate-100 w-full h-full duration-100 ease-linear rounded-md  flex justify-center items-center`}
+                        >
+                        <RotatingLines color="#16b916" width='30px' />
+                      </div> : <></>
+                      }
+                      <img 
+                        src={item} 
+                        alt="" 
+                        loading='lazy'
+                        onLoad={()=>setLoad(true)}
+                        className='w-full h-full'
+                      />
+                    </div>
+                  )
+                })
+              }
+              
 
             </div>
           </div>
@@ -103,19 +115,26 @@ const ProductPage = () => {
         >
           <h1
             className='text-2xl font-medium font-serif'
-          >{"Title"}</h1>
+          >{productData.name}</h1>
           <div 
             className='flex text-[#FACA51] my-2 text-lg mr-2'
           >
-            <MdStar />
-            <MdStar />
-            <MdStar />
-            <MdStar />
-            <MdStar />
+            {
+              productData?.reviews?.length === 0 ? (<>
+                <MdStarHalf />
+                <MdStarOutline />
+                <MdStarOutline />
+                <MdStarOutline />
+                <MdStarOutline />
+              </>
+                ):(
+                <MdStarRate />
+              )
+            }
             <span className='text-center font-semibold text-topBarTextColor text-sm'>
                 (  
                   <small>
-                   { "6" } 
+                   { productData?.reviews?.length } 
                   </small>
                )
             </span>
@@ -125,7 +144,7 @@ const ProductPage = () => {
             className='text-xl font-medium'
           >
             BDT{" "}
-            {"500"}
+            {productData?.price}
           </h2>
 
           <div
@@ -133,53 +152,50 @@ const ProductPage = () => {
           >
             <h2
               className='text-sm mr-4'
-            >Size</h2>
-            <div
-              className='w-[30px] h-[30px] border flex justify-center items-center text-xs my-2 bg-[#5bff1a3b] mr-2'
-            >
-              S
-            </div>
-            <div
-              className='w-[30px] h-[30px] border flex justify-center items-center text-xs my-2 mr-2'
-            >
-              M
-            </div>
-            <div
-              className='w-[30px] h-[30px] border flex justify-center items-center text-xs my-2  mr-2'
-            >
-              L
-            </div>
-            <div
-              className='w-[30px] h-[30px] border flex justify-center items-center text-xs my-2 mr-2'
-            >
-              XL
-            </div>
+            >Size:</h2>
+            {
+              avalableSizes?.map( (item , i ) =>{
+
+                return(
+                  <div
+                    onClick={()=>setSelectedSize(item)}
+                    key={i}
+                    className={`w-[30px] h-[30px] border flex justify-center items-center text-xs my-2  mr-2 uppercase ${ selectedSize === item ? "bg-[#5bff1a3b]" : "bg-transparent"}`}
+                  >
+                    {item}
+                </div>
+                )
+              })
+            }
+            
           </div>
 
           <div 
             className='my-2 font-light'
           >
             <small>
-              ({"20"}) Item's ar avalable
+              ({ productData?.quantity }) Item's ar avalable
             </small>
             <br />
             <strong
             >
-              Categories:  <small>{"T-shirt"}</small>
+              Categories:  <small>{ productData?.category}</small>
             </strong>
           </div>
 
           <div
             className='text-sm mb-5 font-mono'
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores doloribus mo....di aspernatur necessitatibus enim, ducimus provident autem ratione mollitia iste!
+            {
+              productData?.description || "..."
+            }.
           </div>
 
           <div
             className='w-full flex justify-center items-center'
           >
             <button
-              className=' w-[200px] h-[60px] rounded-md text-lg font-semibold mb-4 active:scale-95 bg-mainIconColor duration-100 ease-in-out'
+              className=' w-[150px] h-[55px] rounded-md text-lg font-semibold mb-4 active:scale-95 bg-mainIconColor duration-100 ease-in-out'
             >Add to cart</button>
           </div>
           
