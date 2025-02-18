b11af435bb808f138b5d1bcd16877e881f5987b4
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-18 11:41:33 +0600

    updated log expration

--- Frontend/src/utils/Promis.js
+++ Frontend/src/utils/Promis.js
@@ -74,7 +74,7 @@ const Promis = () => {
                 
                 // send visitor
                 await axios.post(`${import.meta.env.VITE_REACT_SERVER_API}/user/visitor`)
-                .then( res => console.log(res.data))
+                .then( res )
                 .catch(err => console.log("error in sending visitor"))
 
                 if( data?.userType === "admin" ){
